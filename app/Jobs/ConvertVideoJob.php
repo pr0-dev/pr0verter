9b4c44caa6f79b4c7afbaf9ab6ddd84fb2c62231
Insax <m.kram@live.at>
2022-03-07 22:44:52 +0100

    Add interpolation

--- app/Jobs/ConvertVideoJob.php
+++ app/Jobs/ConvertVideoJob.php
@@ -38,6 +38,7 @@ class ConvertVideoJob implements ShouldQueue
             '-level', '4.0',
             '-preset', 'medium',
             '-fs', $this->conversion->result_size . 'k',
+            '-vf', 'minterpolate=fps=60',
             '-movflags', '+faststart'
         ];
         if ($this->conversion->result_audio)
