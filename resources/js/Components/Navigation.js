1e37f59d93a476ea8a724a3821d55920385e91e9
Jonas Breuer <jonas.breuer@asynos.com>
2022-03-09 19:36:21 +0100

    Added Mobile Navigation

--- resources/js/Components/Navigation.js
+++ resources/js/Components/Navigation.js
@@ -1,17 +1,31 @@
-import React from 'react';
+import React, {useState} from 'react';
 import NavigationLink from "@/Components/NavigationLink";
 
 function Navigation(props) {
+    const [expanded, setExpanded] = useState(false);
     return (
-        <div className={"w-full bg-pr0-dark h-20 shadow-lg flex justify-between items-center px-12"}>
-            <div className={""}><h1 className={"text-white text-4xl"}>pr0verter</h1></div>
-            <div className={"flex gap-12 text-white text-2xl"}>
-                <NavigationLink path={"/converter"}>Konvertieren</NavigationLink>
-                <NavigationLink path={"/faq"}>FAQ</NavigationLink>
-                <NavigationLink path={"/contact"}>Kontakt</NavigationLink>
-                <NavigationLink path={"/changelog"}>Changelog</NavigationLink>
+        <React.Fragment>
+            <div className={"w-full bg-pr0-dark h-20 shadow-lg flex justify-between items-center px-8 md:px-12"}>
+                <div className={""}>
+                    <h1 className={"text-white text-4xl"}>pr0verter</h1>
+                </div>
+                <div className={"gap-12 text-white text-2xl hidden md:flex"}>
+                    <NavigationLink path={"/converter"}>Konvertieren</NavigationLink>
+                    <NavigationLink path={"/faq"}>FAQ</NavigationLink>
+                    <NavigationLink path={"/contact"}>Kontakt</NavigationLink>
+                    <NavigationLink path={"/changelog"}>Changelog</NavigationLink>
+                </div>
+                <div className={"w-8 h-8 border-2 border-white md:hidden"} onClick={() => setExpanded(!expanded)}></div>
             </div>
-        </div>
+            {expanded &&
+                <div className={"w-full bg-pr0-dark p-8 pt-0 flex flex-col gap-4 md:hidden"}>
+                    <NavigationLink path={"/converter"}>Konvertieren</NavigationLink>
+                    <NavigationLink path={"/faq"}>FAQ</NavigationLink>
+                    <NavigationLink path={"/contact"}>Kontakt</NavigationLink>
+                    <NavigationLink path={"/changelog"}>Changelog</NavigationLink>
+                </div>
+            }
+        </React.Fragment>
     );
 }
 
--- resources/js/Components/NavigationLink.js
+++ resources/js/Components/NavigationLink.js
@@ -4,7 +4,7 @@ import {Link, usePage} from "@inertiajs/inertia-react";
 function NavigationLink({path, children}) {
     const { url } = usePage()
     return (
-        <Link href={path} className={"text-white text-2xl hover:text-pr0-main duration-100 transition-all " + (url.startsWith(path) && "text-pr0-main")}>{children}</Link>
+        <Link href={path} className={"block text-white text-2xl hover:text-pr0-main duration-100 transition-all " + (url.startsWith(path) && "text-pr0-main")}>{children}</Link>
     );
 }
 
