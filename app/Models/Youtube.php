a4aca5b20d2a1d9dbfc9bc462f8d3e52b8795cf9
Insax <m.kram@live.at>
2022-03-10 14:32:20 +0100

    Add missing model definitions

--- app/Models/Youtube.php
+++ app/Models/Youtube.php
@@ -12,14 +12,24 @@ use Illuminate\Support\Carbon;
  * App\Models\Youtube
  *
  * @property int $id
+ * @property string $url
+ * @property string $subtitle
+ * @property string|null $progress
+ * @property string|null $rate
+ * @property string|null $eta
  * @property Carbon|null $created_at
  * @property Carbon|null $updated_at
  * @method static Builder|Youtube newModelQuery()
  * @method static Builder|Youtube newQuery()
  * @method static Builder|Youtube query()
  * @method static Builder|Youtube whereCreatedAt($value)
+ * @method static Builder|Youtube whereEta($value)
  * @method static Builder|Youtube whereId($value)
+ * @method static Builder|Youtube whereProgress($value)
+ * @method static Builder|Youtube whereRate($value)
+ * @method static Builder|Youtube whereSubtitle($value)
  * @method static Builder|Youtube whereUpdatedAt($value)
+ * @method static Builder|Youtube whereUrl($value)
  * @mixin Eloquent
  */
 class Youtube extends Model
