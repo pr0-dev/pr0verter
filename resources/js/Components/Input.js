687984759eec8cea38d9283cdeaad43c2dcc4eb5
Jonas Breuer <jonas.breuer@asynos.com>
2022-03-11 21:26:58 +0100

    Testing Converter

--- resources/js/Components/Input.js
+++ resources/js/Components/Input.js
@@ -1,6 +1,6 @@
 import React, {useEffect, useState} from 'react';
 
-function Input({placeholder, onChange, className}) {
+function Input({placeholder, onChange, className, min, max, type, inputRef}) {
     const [value, setValue] = useState("");
 
     useEffect(() => {
@@ -12,9 +12,13 @@ function Input({placeholder, onChange, className}) {
     return (
         <div className={className}>
             <input
-                className={"bg-pr0-dark py-3 px-6 text-xl placeholder-pr0-text w-full rounded-2xl text-white shadow-lg"}
+                className={"bg-pr0-dark py-3 px-6 text-xl placeholder-pr0-text w-full rounded-2xl text-white shadow-lg border-0"}
                 placeholder={placeholder}
                 value={value}
+                min={min}
+                max={max}
+                type={type}
+                ref={inputRef}
                 onChange={(e) => setValue(e.target.value)}
             />
         </div>
--- resources/js/Pages/Converter.js
+++ resources/js/Pages/Converter.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import {Head, Link, usePage} from "@inertiajs/inertia-react";
 import Navigation from "@/Components/Navigation";
 import Input from "@/Components/Input";
@@ -6,7 +6,7 @@ import FileUpload from "@/Components/FileUpload";
 import Switch from "@/Components/Switch";
 import Button from "@/Components/Button";
 
-function Converter(props) {
+function Converter() {
     const {url} = usePage()
     const [mode, setMode] = useState(url.endsWith("/youtube") ? 0 : url.endsWith("/download") ? 1 : 2);
     const [source, setSource] = useState("");
@@ -14,15 +14,103 @@ function Converter(props) {
     const [ratio, setRatio] = useState(true);
     const [sound, setSound] = useState(true);
     const [size, setSize] = useState(0);
+    const [bitrate, setBitRate] = useState(0);
     const [interpolation, setInterpolation] = useState(true);
     const [start, setStart] = useState(0);
     const [end, setEnd] = useState(0);
+    const [limits, setLimits] = useState({});
+
+    const sizeRef = useRef();
+    const startRef = useRef();
+    const endRef = useRef();
+
+    useEffect(() => {
+        axios.get(route("settings")).then(data => {
+            setLimits(data.data);
+        }).catch((err) => {
+            console.log(err)
+        })
+    }, [])
+
+    function send() {
+        let failed = false;
+
+        if (limits) {
+            console.log(limits.minResultSize)
+            if (start && end) {
+                if (end - start > limits.maxResultLength || start > end) {
+                    failed = true;
+                    startRef.current.style.border = "2px solid #ff0000";
+                    endRef.current.style.border = "2px solid #ff0000";
+                    // Clip too long
+                } else {
+                    startRef.current.style.border = "none";
+                    endRef.current.style.border = "none";
+                }
+            }
+
+            if (size < limits.minResultSize || size > limits.maxResultSize) {
+                failed = true;
+                sizeRef.current.style.border = "2px solid #ff0000";
+            } else {
+                sizeRef.current.style.border = "none";
+            }
+
+            if (sound) {
+                if (bitrate !== 0 && bitrate < limits.minResultAudtioBitrate) {
+                    failed = true;
+                    // Sound too small
+                }
+
+                if (bitrate > limits.maxAudioBitRate) {
+                    failed = true;
+                    // Sound too big
+                }
+            }
+        } else {
+            failed = true;
+        }
+
+        if (failed) {
+            return;
+        }
+
+        if(mode === 0) {
+            axios.post(route("storeYoutube"), {
+                size: size,
+                resolution: ratio,
+                sound: sound,
+                start: start,
+                end: end,
+                url: source,
+                interpolation: interpolation
+            }).then(data => {
+                console.log(data);
+            }).catch(err => {
+                console.log(err);
+            })
+        }else if(mode === 1) {
+            axios.post(route("storeDownload"), {
+                size: size,
+                resolution: ratio,
+                sound: sound,
+                start: start,
+                end: end,
+                url: source,
+                interpolation: interpolation
+            }).then(data => {
+                console.log(data);
+            }).catch(err => {
+                console.log(err);
+            })
+        }
+    }
 
     return (
         <>
-            <Head title="pr0verter"/>
+            <Head title="pr0verter"><title>pr0verter</title></Head>
             <div className="bg-pr0-bg w-full min-h-screen">
-                <Navigation></Navigation>
+                <Navigation/>
 
                 {
                     // Mode Selection
@@ -58,7 +146,7 @@ function Converter(props) {
                     </div> : mode === 1 ? <div>
                         <Input placeholder={"Download URL..."} onChange={setSource}/>
                     </div> : <div>
-                        <FileUpload></FileUpload>
+                        <FileUpload/>
                     </div>
                     }
                 </div>
@@ -66,7 +154,7 @@ function Converter(props) {
                 {
                     // YouTube specific params
                     mode === 0 &&
-                    <div className={"px-4 mt-8 w-full md:1/3 mx-auto"}>
+                    <div className={"px-4 mt-8 w-full md:w-1/3 mx-auto"}>
                         <div className={"flex justify-between w-full items-center"}>
                             <p className={"text-xl text-pr0-text"}>Untertitel</p>
                             <Switch defaultValue={false} className={"w-32"} onChange={setSubtitle}/>
@@ -87,23 +175,28 @@ function Converter(props) {
                     </div>
                     <div className={"flex justify-between mt-8 w-full items-center"}>
                         <p className={"text-xl text-pr0-text"}>Wunschgröße</p>
-                        <Input placeholder={"Größe in MB"} className={"w-1/2"} onChange={setSize}/>
+                        <Input placeholder={"Größe in MB"} type={"number"} className={"w-1/2"} onChange={setSize}
+                               inputRef={sizeRef}/>
                     </div>
                     <div className={"flex justify-between mt-8 w-full items-center"}>
                         <p className={"text-xl text-pr0-text"}>Schnitt Start</p>
-                        <Input placeholder={"Sekunden"} className={"w-1/2"} onChange={setStart}/>
+                        <Input type={"number"} placeholder={"Sekunden"} className={"w-1/2"} onChange={setStart}
+                               inputRef={startRef}/>
                     </div>
                     <div className={"flex justify-between mt-8 w-full items-center"}>
                         <p className={"text-xl text-pr0-text"}>Schnitt Ende</p>
-                        <Input placeholder={"Sekunden"} className={"w-1/2"} onChange={setEnd}/>
+                        <Input placeholder={"Sekunden"} type={"number"} className={"w-1/2"} onChange={setEnd}
+                               inputRef={endRef}/>
                     </div>
-                    <div className={"flex justify-between w-full mt-8 items-center"}>
-                        <p className={"text-xl text-pr0-text"}>Interpolation</p>
-                        <Switch defaultValue={false} className={"w-32"} onChange={setInterpolation}/>
-                    </div>
+                    {(limits.disabled && !limits.disabled.inputs.interpolation) &&
+                        <div className={"flex justify-between w-full mt-8 items-center"}>
+                            <p className={"text-xl text-pr0-text"}>Interpolation</p>
+                            <Switch defaultValue={false} className={"w-32"} onChange={setInterpolation}/>
+                        </div>
+                    }
                 </div>
                 <div className={"px-4 w-full md:w-1/3 mt-8 mx-auto"}>
-                    <Button className={"w-full md:w-1/2"}>Konvertieren</Button>
+                    <Button className={"w-full md:w-1/2"} onClick={send}>Konvertieren</Button>
                 </div>
             </div>
         </>
