890bdaa62d5abe4cdb4fa382f0c7356e75c9eb10
Insax <m.kram@live.at>
2022-03-29 17:40:02 +0200

    Update Kernel.php

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -64,7 +64,7 @@ class Kernel extends HttpKernel
 
         'api' => [
             // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
-            'throttle:api',
+            // 'throttle:api',
             SubstituteBindings::class,
         ],
     ];
