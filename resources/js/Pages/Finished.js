f17db86d535ef8e1764bacf4564cff5676aaa0de
Jonas Breuer <jonas.breuer@asynos.com>
2022-03-29 18:27:28 +0200

    added autoplay

--- resources/js/Pages/Finished.js
+++ resources/js/Pages/Finished.js
@@ -10,7 +10,7 @@ export default function Finished(props) {
             <Head title="pr0verter" />
             <div className="bg-pr0-bg w-full min-h-screen">
                 <Navigation/>
-                <video className={"w-1/2 mx-auto mt-28 bg-black shadow-lg rounded-2xl"}>
+                <video controls className={"w-1/2 mx-auto mt-28 bg-black shadow-lg rounded-2xl"}>
                     <source src={route("viewConversion", props.conversion.guid)} type="video/mp4" />
                 </video>
                 <p className={"mx-auto text-center mt-6 text-xl text-pr0-main p-3 bg-pr0-dark w-1/2 rounded-2xl shadow-lg"}>{route("downloadConversion", props.conversion.guid)}</p>
