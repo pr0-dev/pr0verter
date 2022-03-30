a18648865a705f386e589b900640eec8db1c4b47
Insax <m.kram@live.at>
2022-03-30 02:31:37 +0200

    Fix Viewing for some browsers (test)

--- resources/js/Pages/Finished.js
+++ resources/js/Pages/Finished.js
@@ -11,7 +11,7 @@ export default function Finished(props) {
             <div className="bg-pr0-bg w-full min-h-screen">
                 <Navigation/>
                 <video controls className={"w-1/2 mx-auto mt-28 bg-black shadow-lg rounded-2xl"}>
-                    <source src={route("viewConversion", props.conversion.guid)} type="video/mp4" />
+                    <source src={route("downloadConversion", props.conversion.guid)} type="video/mp4" />
                 </video>
                 <p className={"mx-auto text-center mt-6 text-xl text-pr0-main p-3 bg-pr0-dark w-1/2 rounded-2xl shadow-lg"}>{route("downloadConversion", props.conversion.guid)}</p>
                 <Button className={"w-1/4 mx-auto mt-6"}>Herunterladen</Button>
