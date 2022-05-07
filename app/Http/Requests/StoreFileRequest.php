e8ee0b15b9e78d9e06ed0505063327f154f40db8
Marcus Kram <github@1ns.at>
2022-05-07 18:15:53 +0200

    Update StoreFileRequest.php

--- app/Http/Requests/StoreFileRequest.php
+++ app/Http/Requests/StoreFileRequest.php
@@ -35,7 +35,7 @@ class StoreFileRequest extends FormRequest
             'end' => ['filled', 'bail', 'integer', new ClipEndRule],
             'ratio' => 'required|bail|boolean',
             config('pr0verter.disabled.inputs.interpolation') ? : 'interpolation' => 'required|bail|boolean',
-            'video' => ['required', 'bail', 'file|min:' . config('pr0verter.minUploadSize'), 'max:' . config('pr0verter.maxUploadSize'), 'mimetypes:video/x-ms-asf,video/x-flv,video/mp4,application/x-mpegURL,video/MP2T,video/3gpp,video/quicktime,video/x-msvideo,video/x-ms-wmv,video/avi,video/x-matroska', new IsVideo]
+            'video' => ['required', 'bail', 'file', 'min:' . config('pr0verter.minUploadSize'), 'max:' . config('pr0verter.maxUploadSize'), 'mimetypes:video/x-ms-asf,video/x-flv,video/mp4,application/x-mpegURL,video/MP2T,video/3gpp,video/quicktime,video/x-msvideo,video/x-ms-wmv,video/avi,video/x-matroska', new IsVideo]
         ];
     }
 }
